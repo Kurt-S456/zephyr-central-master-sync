cf6b6f1d19d54729c141c63dd08991e7835fce2a
kurtS <kurt.florian.schrammel@proton.me>
2026-08-02 14:21:35 +0200

    doc: benchmark run

--- scripts/calc_metrics.py
+++ scripts/calc_metrics.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Calculate timing metrics from child log output.
 
-Expected child log line format:
+Expected child log line formats:
 CHILD <id> offset: <signed_us> us | synced: <ms_int>.<ms_frac_6> ms
+CHILD <id> offset: <signed_us> us | pulse_to_spi: <signed_us> us | synced: <ms_int>.<ms_frac_6> ms
 
 Notes:
 - In current firmware, `offset` is (worker_ts - child_local_ts) in microseconds.
@@ -23,6 +24,7 @@ from typing import Dict, Iterable, List, Optional
 
 LOG_RE = re.compile(
     r"CHILD\s+(?P<child_id>\d+)\s+offset:\s+(?P<offset_us>-?\d+)\s+us\s+\|\s+"
+    r"(?:pulse_to_spi:\s+-?\d+\s+us\s+\|\s+)?"
     r"synced:\s+(?P<sync_ms_int>\d+)\.(?P<sync_ms_frac>\d{6})\s+ms"
 )
 
